889d5f2296e5d4866e2ab249d555502493830d68
tamim wasif <40170742+tamimjdd@users.noreply.github.com>
2022-01-07 09:44:48 +0600

    search-notification
    search-notification

---
+++ search-notification-realtieme/tamim-web2/resources/views/posts/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="inline-grid grid-cols-3 gap-4">
+    @foreach($posts as $post)
+        <div class="pt-4">
+            <a href="/p/{{$post->id}}">
+                <img src="{{ asset('images/' . $post->thumbnail) }}" class="w-4/5">
+                <div class="flex pt-4">
+                    <img src="{{ $post->user->profile->profileImage()}}" class="w-6 rounded-full lg:w-14" alt="">
+
+                    <div class="text-sm lg:text-2xl">{{$post->title}}</div >
+                </div>
+            </a>
+        </div>
+    @endforeach
+
+</div>
+
+
+@endsection
